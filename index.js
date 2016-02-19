733278e022df56f7bf5e1a6e66f292f1467ef417
Mariusz Zak <mariusz.zak@ironin.pl>
2016-02-19 15:39:48 +0100

    Removed lines which caused errors while building application.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 module.exports = {
   name: 'ember-autoresize',
   included: function (app) {
-    app.options.snippetSearchPaths = ['tests/dummy/app'];
-    app.options.snippetPaths = ['tests/dummy/snippets'];
+    //app.options.snippetSearchPaths = ['tests/dummy/app'];
+    //app.options.snippetPaths = ['tests/dummy/snippets'];
 
     this._super.included(app);
     app.import('bower_components/dom-ruler/dist/dom-ruler.amd.js', {
